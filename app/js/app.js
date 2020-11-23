90daf125c22c9c051e6895560395084a612cd0e9
Skripkaru Evgeniy <skripkaru.evgeniy@gmail.com>
2020-11-23 20:41:43 +0300

    Якоря, модалка, мелкие правки

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,37 @@
+// Slider
 const mySwiper = new Swiper('.swiper-container', {
     autoplay: {
         delay: 5000,
         disableOnInteraction: false,
     },
     effect: 'fade',
+})
+
+// Scroll
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+// Modal
+$(document).on('scroll', function () {
+    if ($(window).scrollTop() > $('.benefits').scrollTop()) {
+        $('.modal').show()
+    } else {
+        $('.modal').hide()
+    }
+})
+
+$('.modal__close').on('click', function (e) {
+    $('.modal').hide()
 })
