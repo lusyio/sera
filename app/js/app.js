b215fe2c65db742f6eea76e35fd434d508b61782
Skripkaru Evgeniy <skripkaru.evgeniy@gmail.com>
2020-11-25 15:52:39 +0300

    Правки

--- app/js/app.js
+++ app/js/app.js
@@ -11,27 +11,53 @@ const mySwiper = new Swiper('.swiper-container', {
 const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
 
-    const blockID = anchor.getAttribute('href').substr(1)
+        const blockID = anchor.getAttribute('href').substr(1)
 
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
     })
-  })
 }
 
 // Modal
-$(document).on('scroll', function () {
-    if ($(window).scrollTop() > $('.benefits').scrollTop()) {
-        $('.modal').show()
+
+const modal = document.querySelector('.modal')
+const benefits = document.querySelector('.benefits')
+const closeBtn = document.querySelector('.modal__close')
+
+window.addEventListener('scroll', function () {
+    // document.getElementById('showScroll').innerHTML = window.pageYOffset + 'px'
+
+    // Расстояние от края окна
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    console.log(scrollTop)
+    // Расстояние от края окна до элемента
+    let distance = benefits.getBoundingClientRect()
+    console.log(distance)
+
+    if (scrollTop > distance) {
+        modal.classList.add('show')
     } else {
-        $('.modal').hide()
+        modal.classList.remove('show')
     }
 })
 
-$('.modal__close').on('click', function (e) {
-    $('.modal').hide()
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('show')
 })
+
+// $(document).on('scroll', function () {
+//     if ($(window).scrollTop() > $('.benefits').scrollTop()) {
+//         $('.modal').show()
+//     } else {
+//         $('.modal').hide()
+//     }
+// })
+
+// $('.modal__close').on('click', function (e) {
+//     $('.modal').hide()
+// })
